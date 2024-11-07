ab7b4cc0a0cfb39bcc71e8a158ed43e31947ba0e
viana2209 <163418959+viana2209@users.noreply.github.com>
2024-11-07 09:58:18 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let numero1 =parseInt (prompt("informe o primeiro numero1:"));
+let numeoro2 = parseInt (prompt("informe o numero 2: "));
+
+let soma =numero1 + numeoro2;
+
+document.write("a soma é igual a:",soma);
+console.log("o valor da soma no console é igual a:,soma");
